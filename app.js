ef6c62c6269025c2980feadcd4ceff51adfe8517
Davi F. Pinheiro <contatodavifariasp@gmail.com>
2022-11-02 14:14:57 -0300

    alterando app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors()) //habilitando cors
 
 app.use('/users', usersRouter);
